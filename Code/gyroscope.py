6a763361810929410a1ab01a5a876a843f5d03d5
Pi <lukas.wombacher@web.de>
2023-05-04 23:36:45 +0200

    ultrasonicsensor and gyroscope

---
+++ Code/gyroscope.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+import json
+import time
+from mpu9250_i2c import *
+from threading import Thread
+time.sleep(1) # delay necessary to allow mpu9250 to settle
+
+with open("settings.json", "r") as settings_file:
+    settings_data = json.load(settings_file)
+    bias_x = settings_data["gyroscope_drift"]["x"]
+    bias_y = settings_data["gyroscope_drift"]["y"]
+    bias_z = settings_data["gyroscope_drift"]["z"]
+
+counter = {"round": 0, "sum_x": 0, "sum_y": 0, "sum_z": 0}
+recording = True
+stop_recording = False
+start = 0
+end = 0
+wz = 1
+
+def record_degree():
+    while True:
+        global counter, wz, start, recording, stop_recording
+        counter = {"round": 0, "sum_x": 0, "sum_y": 0, "sum_z": 0}
+        start = time.time()
+        while recording:
+            try:
+                ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+                wz -= bias_z
+                #if -0.3 < wz < 0.3:
+                #    wz = 0
+                    
+            except:
+                continue
+            counter["round"] += 1
+            counter["sum_z"] += wz
+        stop_recording = True
+
+def get_abs_degree():
+    end = time.time()
+    global start, counter, degree_z
+    if counter["round"] != 0:
+        degree_z = (counter["sum_z"] / counter["round"]) * (end - start)
+    else:
+        degree_z = 0
+    return degree_z
+
+def get_current_dps():
+    print('gyro [dps]: z = ' + str(wz))
+    return wz
+
+def restart():
+    global stop_recording, recording
+    recording = False
+    while not(stop_recording):
+        time.sleep(0.01)
+    stop_recording = False
+    recording = True
+
+
+def test():
+    while True:
+        print(get_abs_degree())
+    
+
+#thread_1 = Thread(target=record_degree)
+#thread_2 = Thread(target=test)
+#thread_1.start()
+#thread_2.start()
---
+++ Code/ultrasonic.py
@@ -0,0 +1,50 @@
+from pydoc import tempfilepager
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+import edit_json
+import math
+
+
+#setup ultrasonic sensors
+
+pin_trigger = {
+"ultrasonic_front": edit_json.get_pin_ports("ultrasonic_front")[1],
+"ultrasonic_left": edit_json.get_pin_ports("ultrasonic_left")[0],
+"ultrasonic_right": edit_json.get_pin_ports("ultrasonic_right")[0]
+}
+pin_echo = {
+"ultrasonic_front": edit_json.get_pin_ports("ultrasonic_front")[0],
+"ultrasonic_left": edit_json.get_pin_ports("ultrasonic_left")[1],
+"ultrasonic_right": edit_json.get_pin_ports("ultrasonic_right")[1]
+}
+
+for single_pin_trigger in pin_trigger.items():
+    GPIO.setup(single_pin_trigger[1], GPIO.OUT)
+    GPIO.output(single_pin_trigger[1], 0)
+
+for single_pin_echo in pin_echo.items():
+    GPIO.setup(single_pin_echo[1], GPIO.IN)
+
+#calibrate speed of sound
+
+temperature = edit_json.get_variable("temperature")
+speed_of_sound = 33150 * math.sqrt(1 + (temperature / 273.15))
+
+"""
+return the distance of the given sensor in cm with decimal places [default value = 1]
+"""
+
+def get_distance(sensor, decimal_places=1):
+    GPIO.output(pin_trigger[sensor], True)
+    time.sleep(0.00001)
+    GPIO.output(pin_trigger[sensor], False)
+    start_time = time.time()
+    end_time = time.time()
+    while GPIO.input(pin_echo[sensor]) == 0:
+        start_time = time.time()
+    while GPIO.input(pin_echo[sensor]) == 1:
+        end_time = time.time()
+    delay = end_time - start_time
+    distance = (delay * speed_of_sound) / 2
+    return round(distance, decimal_places)
