30d1ffc06e87bf34587aa5436ea8db99d23a150f
Pi <lukas.wombacher@web.de>
2023-05-24 15:34:19 +0200

    ultrasonic upgrades and testing

--- Code/eroeffnungsrennen.py
+++ Code/eroeffnungsrennen.py
@@ -11,11 +11,11 @@ curve_count = 0
 running = True
 true_count_bigger = [0, 0, 0, 0, 0, 0, 0, 0]
 true_count_smaler = [0, 0, 0, 0, 0, 0, 0, 0]
-curve_goal = 4
+curve_goal = 4*1
 
-def ultrasonic_savety_bigger(sensor, distance, num=0):
+def ultrasonic_savety_bigger(sensor, distance, num):
     global true_count_bigger
-    if ultrasonic.get_distance(sensor) > distance:
+    if ultrasonic.get_distance(sensor) >= distance:
         true_count_bigger[num] += 1
     else:
         true_count_bigger[num] = 0
@@ -25,9 +25,9 @@ def ultrasonic_savety_bigger(sensor, distance, num=0):
     else:
         return False
 
-def ultrasonic_savety_smaler(sensor, distance, num=0):
+def ultrasonic_savety_smaler(sensor, distance, num):
     global true_count_smaler
-    if ultrasonic.get_distance(sensor) < distance:
+    if ultrasonic.get_distance(sensor) <= distance:
         true_count_smaler[num] += 1
     else:
         true_count_smaler[num] = 0
@@ -39,30 +39,29 @@ def ultrasonic_savety_smaler(sensor, distance, num=0):
 
 Kp = 0.04  # Proportionalitätskonstante
 Ki = 0.015  # Integral-Konstante
-Kd = 0.5  # Differential-Konstante
+Kd = 0  # Differential-Konstante
 prev_error = 0  # vorheriger Fehler
 integral = 0  # Summe der Fehler über die Zeit
 
+def pid_reset():
+    Kp = 0.04  # Proportionalitätskonstante
+    Ki = 0.015  # Integral-Konstante
+    Kd = 0  # Differential-Konstante
+    prev_error = 0  # vorheriger Fehler
+    integral = 0  # Summe der Fehler über die Zeit
+
 def accurate():
     global curve_count, direction, Kp, Ki, Kd, prev_error, integral
-    lenk = (gyroscope.get_abs_degree() )#nur im ganzen Programm+ (90*curve_count*((2*direction)-1)))
+    print("PID start")
+    lenk = (gyroscope.get_abs_degree() + (90*curve_count*((2*direction)-1)))
     error = lenk  # aktueller Fehler
     derivative = error - prev_error  # Ableitung des Fehlers
     integral = integral*0.5 + error  # Summe der Fehler aktualisieren
     prev_error = error  # vorherigen Fehler aktualisieren
     correction = Kp * error + Ki * integral + Kd * derivative  # Korrekturwert berechnen
-    """if correction > 45:
-        correction = 45
-    elif correction < -45:
-        correction = -45"""
     pid_direction = "right" if correction > 0 else "left"  # Richtung basierend auf der Korrektur wählen
     stepper_motor.turn_distance(45, abs(correction), pid_direction)  # Ansteuerung des Lenkmotors
-    """if temp_deg < -1:
-        stepper_motor.turn_distance(100, lenk, "left")
-        stepper_motor.turn_distance(100, lenk, "right")
-    elif temp_deg > 1:
-        stepper_motor.turn_distance(100, lenk, "right")
-        stepper_motor.turn_distance(100, lenk, "left")"""
+    print("PID end")
 
 def curve():
     global direction, d_switch, curve_count
@@ -79,24 +78,28 @@ def curve():
 def main():
     global direction, d_switch, curve_count, running, curve_goal
     gyroscope.restart()
-    drive_motor.speed = 70
+    drive_motor.speed = 75
     print("start")
     while direction == 2:
-        if ultrasonic_savety_bigger("ultrasonic_left", 200, 0):
+        if ultrasonic_savety_bigger("ultrasonic_left", 150, 0):
             direction = 0
-        elif ultrasonic_savety_bigger("ultrasonic_right", 200, 1):
+        time.sleep(0.01)
+        if ultrasonic_savety_bigger("ultrasonic_right", 150, 1):
             direction = 1
-        print("tested")
-    print(d_switch[direction])
+        time.sleep(0.01)
+        
+        print(d_switch[direction])
     curve()
     while running:
-        while (not ultrasonic_savety_smaler("ultrasonic_front", 100, 0)) or (not ultrasonic_savety_bigger("ultrasonic_"+d_switch[direction], 200, 2)):
-            #time.sleep(0.001)
-            accurate()
+        pid_reset()
+        #while (not ultrasonic_savety_smaler("ultrasonic_front", 150, 0)) or (not ultrasonic_savety_bigger("ultrasonic_"+d_switch[direction], 200, 2)):
+            #accurate()
+        while (not ultrasonic_savety_smaler("ultrasonic_front", 50, 1)) or (not ultrasonic_savety_bigger("ultrasonic_"+d_switch[direction], 200, 3)):
+            time.sleep(0.01)
         curve()
         if curve_count >= curve_goal:
-            while not ultrasonic_savety_smaler("ultrasonic_front", 150, 1):
-                time.sleep(0.1)
+            while not ultrasonic_savety_smaler("ultrasonic_front", 150, 2):
+                accurate()
             drive_motor.speed = 0
             running = False
 
@@ -107,14 +110,12 @@ def main():
 def main2():
     gyroscope.restart()
     print("Ready")
-    #time.sleep(3)
     drive_motor.speed = 75
     while True:
         accurate()
-        print("PID")
 
 thread_1 = Thread(target=gyroscope.record_degree)
-thread_2 = Thread(target=main2)
+thread_2 = Thread(target=main)
 thread_3 = Thread(target=drive_motor.on)
 
 thread_1.start()
--- Code/test.py
+++ Code/test.py
@@ -1,15 +1,7 @@
-import ultrasonic
 import time
-kp = 0
-#while True:
-#    print(ultrasonic.get_distance("ultrasonic_right"))
-#    time.sleep(0.1)
-
-def neu():
-    global kp
-    kp += 1
-    print(kp)
-
-neu()
-neu()
-neu()
+import ultrasonic
+print("start")
+while True:
+    print(ultrasonic.get_distance("ultrasonic_left", 3))
+    time.sleep(0.2)
+    
