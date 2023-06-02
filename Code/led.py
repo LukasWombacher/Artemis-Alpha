58cccbfeb745bda7eb4dbc4d89ec5a47949d6a13
Pi <lukas.wombacher@web.de>
2023-06-02 16:25:47 +0200

    led update

--- Code/led.py
+++ Code/led.py
@@ -12,7 +12,8 @@ import edit_json
 
 # LED strip configuration:
 LED_COUNT = 22        # Number of LED pixels.
-LED_PIN = 7 #edit_json.get_pin_ports("led_strip")[0]          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = edit_json.get_pin_ports("led_strip")[0]          # GPIO pin connected to the pixels (18 uses PWM!).
+print(LED_PIN)
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
